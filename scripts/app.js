7fd38648cd63e218cd4fd6bd3d9eda2c89bdc323
David Olorundare <david.o@ieee.org>
2018-07-03 10:57:55 -0500

    updated app.js file

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,8 @@
-var platform = new H.service.Platform({ app_id: 'avtYhQpLdMWucRoHrQyO', app_code: '3fJ3dzihIBjmmFQjRdk0ng'});
+var platform = new H.service.Platform({ app_id: "{avtYhQpLdMWucRoHrQyO}", app_code: "{3fJ3dzihIBjmmFQjRdk0ng}"});
 
 var defaultLayers = platform.createDefaultLayers();
 var mapPlaceholder = document.getElementById('map-container');
 
-var map = new H.Map(mapContainer, defaultLayers.normal.map);
+var map = new H.Map(mapPlaceholder, defaultLayers.normal.map);
 
 window.addEventListener('resize', function(){ map.getViewPort().resize();});
